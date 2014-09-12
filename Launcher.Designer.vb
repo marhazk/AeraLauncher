8274e3eb4e4f3d2d9b1565c00393e42f01432b72
marhazk <marhazk@yahoo.com>
2014-09-12 19:56:45 +0800

    default

---
+++ Launcher.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Launcher
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Launcher))
+        Me.SuspendLayout()
+        '
+        'Launcher
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImage = Global.AeraLauncher.My.Resources.Resources.eubo
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.CausesValidation = False
+        Me.ClientSize = New System.Drawing.Size(331, 258)
+        Me.ControlBox = False
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Launcher"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "PW:Aera Launcher"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+End Class
---
+++ Launcher.vb
@@ -0,0 +1,16 @@
+﻿Public Class Launcher
+
+    Private Sub Launcher_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Thread current = Thread.CurrentThread;
+        Thread.Sleep(3000);
+        Try
+			{
+				Process newProc = Process.Start("iexplore.exe", forumaddr);
+			}
+        Catch
+			{
+				MessageBox.Show("Internet Explorer is not found", "Error!",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+    End Sub
+End Class
